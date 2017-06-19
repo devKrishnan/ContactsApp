ef1c10b11ae47b29207df7b20be815faf1c76f29
radhakrishnan S <krish.selvaraj@gmail.com>
2017-06-19 10:59:28 +0530

    Added sample data to store for testing

--- app/reducer.js
+++ app/reducer.js
@@ -11,6 +11,16 @@ const initialState = [{
     "favorite": true,
     "created_at": "2017-06-09T11:18:08.511Z",
     "updated_at": "2017-06-18T08:50:33.808Z"
+},{
+    "id": 226,
+    "first_name": "Batman",
+    "last_name": " Robin",
+    "email": "abc@gmail.com",
+    "phone_number": "9038524128",
+    "profile_pic": "/images/missing.png",
+    "favorite": true,
+    "created_at": "2017-06-09T11:18:08.511Z",
+    "updated_at": "2017-06-18T08:50:33.808Z"
 }]
 
 const contacts = (state = initialState, action) => {
